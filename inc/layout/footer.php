7195b7ecfeb6f8780a30d868da1bbc3f30efec0e
Mifene95 <mifene95@gmail.com>
2026-04-25 09:52:48 +0200

    script js crear_platos.php

--- inc/layout/footer.php
+++ inc/layout/footer.php
@@ -2,8 +2,15 @@
     <strong>Hotel Buffet &copy; 2026.</strong>
 </footer>
 </div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="../js/scripts.js"></script>
+
 </body>
 
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//CREACION NUEVO PLATO
+$(document).ready(function(){
+$('#formNuevoPlato').submit(function(e){
+    e.preventDefault();
+
+    //SERIALIZE JUNTA TODO
+    var datos = $(this).serialize();
+    console.log(datos);
+
+    $.ajax ({
+        url: '../inc/creacion_platos.php',
+        method: 'POST',
+        data: datos,
+
+        success: function(respuesta){
+            
+            if(respuesta === "ok"){
+                Swal.fire({
+                    title: "¡Guardado!",
+                    text: "El plato se ha creado correctamente",
+                    icon: "success",
+                    confirmButtonText: "Genial"
+                });
+                //LIMPIAMOS FORM
+                $('#formNuevoPlato')[0].reset();
+            }else{
+                Swal.fire({
+                    title: "Error",
+                    text: "Hubo un problema: " + respuesta,
+                    icon: "error"
+                })
+            }
+        }
+    })
+
+});
+
+});
